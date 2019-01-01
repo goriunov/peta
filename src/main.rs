8afbedd61b9b2c992130a0a914bc1b568cc05724
Dmitrii Goriunov <goriunovd@gmail.com>
2019-01-02 01:21:55 +1300

    Cleanup router system

--- src/main.rs
+++ src/main.rs
@@ -18,39 +18,51 @@ struct Person {
   last_name: String,
 }
 
+fn home(req: peta::request::Request) -> peta::router::ReturnFuture {
+  let mut res = peta::response::Response::new();
+  res.status(peta::status::OK);
+  res.body_str(req.uri().path());
+
+  Box::new(futures::future::ok(res))
+}
+
+fn delay(req: peta::request::Request) -> peta::router::ReturnFuture {
+  let when = Instant::now() + Duration::from_millis(2000);
+
+  let delay = Delay::new(when)
+    .map_err(|e| panic!("Delay errored; err={:?}", e))
+    .and_then(move |_| {
+      let mut res = peta::response::Response::new();
+      res.status(peta::status::OK);
+      res.body_str("Hello world!");
+      Ok(res)
+    });
+
+  Box::new(delay)
+}
+
+fn not_found(req: peta::request::Request) -> peta::router::ReturnFuture {
+  let mut res = peta::response::Response::new();
+  res.status(peta::status::OK);
+  res.body_str("Did not found page");
+
+  Box::new(futures::future::ok(res))
+}
+
 fn main() {
   let mut runtime = tokio::runtime::current_thread::Runtime::new().unwrap();
   let addr = "127.0.0.1:3000".parse().unwrap();
 
   let listener = TcpListener::bind(&addr).expect("unable to bind TCP listener");
 
-  let mut router = peta::router::Router::new();
-
-  router.add(String::from("/home"), move |req| {
-    // println!("Query {}", req.uri().query().unwrap_or("None"));
-    // println!("Path {}", req.method());
-    let mut res = peta::response::Response::new();
-    res.status(peta::status::OK);
-    res.body_str(req.uri().path());
-
-    Box::new(futures::future::ok(res))
-  });
-
-  router.add(String::from("/delay"), move |req| {
-    let when = Instant::now() + Duration::from_millis(2000);
-
-    let delay = Delay::new(when)
-      .map_err(|e| panic!("Delay errored; err={:?}", e))
-      .and_then(move |_| {
-        let mut res = peta::response::Response::new();
-        res.status(peta::status::OK);
-        res.body_str("Hello world!");
-        Ok(res)
-      });
-
-    Box::new(delay)
-  });
   // build router
+  let mut router = peta::router::Router::new();
+  // does not take routes order in account yet
+  router.add("/home", home);
+  router.add("/delay", delay);
+  router.add("/delay/*", home);
+  // we must provide "*" route // as a default response
+  router.add("*", not_found);
   let router = router.build();
 
   let server = listener
@@ -61,31 +73,11 @@ fn main() {
 
       // get arc pointer
       let router = router.clone();
-      // need to write boxed future
       let reader = peta::reader::HttpReader::new(read)
         .map_err(|e| println!("Error is: {}", e))
         .fold(write, move |writer, req| {
-          // let mut res = peta::response::Response::new();
-          // res.status(peta::status::OK);
-          // res.body_str(req.uri().path());
-
-          match router.find(req.uri().path()) {
-            Some(func) => (func)(req),
-            None => {
-              (router.find("/home").unwrap())(req)
-
-              // let mut res = peta::response::Response::new();
-              // res.status(peta::status::OK);
-              // res.body_str(req.uri().path());
-              // Box::new(futures::future::ok(res))
-              //   .and_then(|res| res.write(writer).map_err(|e| println!("Error: {}", e)))
-            }
-          }
-          .and_then(|rsp| rsp.write(writer).map_err(|e| println!("Error: {}", e)))
-
-          // res.write(writer).map_err(|e| println!("Error: {}", e))
-          // (router.func.as_ref().unwrap())(req)
-          //   .and_then(|rsp| rsp.write(writer).map_err(|e| println!("Error: {}", e)))
+          router.find(req.uri().path())(req)
+            .and_then(|resp| resp.write(writer).map_err(|e| println!("Error: {}", e)))
         })
         .map(|_| ());
 
@@ -99,41 +91,3 @@ fn main() {
 
   // tokio::run(server);
 }
-
-// fn on_delay(
-//   res: peta::request::Request,
-// ) -> Box<Future<Item = peta::response::Response, Error = ()> + Send + Sync> {
-//   let when = Instant::now() + Duration::from_millis(2000);
-
-//   let delay = Delay::new(when)
-//     .map_err(|e| panic!("Delay errored; err={:?}", e))
-//     .and_then(move |_| {
-//       let mut res = peta::response::Response::new();
-//       res.status(peta::status::OK);
-//       res.body_str("Hello world!");
-//       Ok(res)
-//     });
-
-//   Box::new(delay)
-// }
-
-// fn process<'a, S: AsyncWrite + 'a>(
-//   write: S,
-//   req: peta::request::Request,
-// ) -> Box<Future<Item = S, Error = ()> + 'a> {
-//   let mut res = peta::response::Response::new();
-//   res.status(peta::status::OK);
-//   res.body_str("Hello world!");
-
-//   // let user = Person {
-//   //   name: "Dmitrii".to_string(),
-//   //   last_name: "Hello".to_string(),
-//   // };
-
-//   // let json = serde_json::to_vec(&user).unwrap();
-//   // res.body_vec(json);
-
-//   // println!("Is working");
-
-//   Box::new(res.write(write).map_err(|e| println!("Error: {}", e)))
-// }
--- src/router.rs
+++ src/router.rs
@@ -1,5 +1,4 @@
 // implement router logic
-// Now i can write nice router logic
 use crate::request;
 use crate::response;
 
@@ -7,6 +6,8 @@ use hashbrown;
 use tokio::prelude::*;
 
 use std::sync::Arc;
+
+pub type ReturnFuture = Box<dyn Future<Item = response::Response, Error = ()> + Send + Sync>;
 
 type StoreFunc = Box<
   dyn Fn(request::Request) -> Box<dyn Future<Item = response::Response, Error = ()> + Send + Sync>
@@ -16,166 +17,133 @@ type StoreFunc = Box<
 
 pub struct Node {
   method: Option<StoreFunc>,
-  children: Option<hashbrown::HashMap<String, Node>>,
+  children: Option<hashbrown::HashMap<&'static str, Node>>,
 }
 
 impl std::fmt::Debug for Node {
   fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-    write!(f, "Node {{ \n children: {:#?} \n}}", self.children)
+    write!(
+      f,
+      "Node {{ \n children: {:#?}, \n method: {:#?} \n}}",
+      self.children,
+      self.method.is_some()
+    )
   }
 }
 
 #[derive(Debug)]
 pub struct Router {
-  routes: Option<Node>,
+  routes: Node,
 }
 
 impl Router {
   pub fn new() -> Router {
-    Router { routes: None }
+    Router {
+      routes: Node {
+        method: None,
+        children: None,
+      },
+    }
   }
 
   pub fn build(self) -> Arc<Self> {
     Arc::new(self)
   }
 
-  pub fn find(&self, path: &str) -> Option<&StoreFunc> {
-    let mut node = self.routes.as_ref().unwrap();
-    for item in path.split('/') {
-      match &node.children {
-        Some(found) => match found.get(&String::from(item)) {
-          Some(exist) => {
-            node = exist;
+  pub fn find(&self, path: &str) -> &StoreFunc {
+    // !! we need to do a lot of optimization for search
+    // and add additional router parsing things
+    let mut node = &self.routes;
+
+    for seg in path.split('/') {
+      if seg.len() > 0 {
+        if node.children.is_none() {
+          break;
+        }
+
+        let children = node.children.as_ref().unwrap();
+
+        let mut found_node = children.get(seg);
+
+        if found_node.is_none() {
+          // if we found at least star then load star route
+          found_node = children.get("*");
+          if found_node.is_some() {
+            node = found_node.unwrap();
           }
-          None => {}
-        },
-        None => {}
+
+          break;
+        }
+
+        node = found_node.unwrap();
       }
     }
 
-    // match node.method {
-    //   Some(f) => f.as_ref(),
-    //   None
-    // }
-
-    node.method.as_ref()
+    match node.method.as_ref() {
+      Some(func) => (func),
+      None => {
+        // if none then load 404 route
+        (self.routes.method.as_ref().unwrap())
+      }
+    }
   }
 
-  pub fn add<F>(&mut self, path: String, func: F)
+  pub fn add<F>(&mut self, path: &'static str, func: F)
   where
     F: Fn(request::Request) -> Box<Future<Item = response::Response, Error = ()> + Send + Sync>
       + Send
       + Sync
       + 'static,
   {
-    match &self.routes {
-      Some(node) => {}
-      None => {
-        self.routes = Some(Node {
-          method: None,
-          children: None,
-        });
-      }
-    }
+    let mut node = &mut self.routes;
 
-    let mut node = self.routes.as_mut().unwrap();
-    // let mut node: Option<Node> = None;
+    if path == "*" {
+      set_func(node, Box::new(func));
+      // if it is stark then set default
+      return;
+    }
 
     for item in path.split('/') {
       if item.len() > 0 {
-        Router::loop_add(node, String::from(item));
+        add_node(node, item);
+        // set Node to next level
         node = node.children.as_mut().unwrap().get_mut(item).unwrap();
       }
     }
 
-    Router::set_fn(node, func);
-
-    println!("{:#?}", self);
+    set_func(node, Box::new(func));
+    // println!("{:#?}", self);
   }
+}
 
-  fn set_fn<F>(node: &mut Node, func: F)
-  where
-    F: Fn(request::Request) -> Box<Future<Item = response::Response, Error = ()> + Send + Sync>
-      + Send
-      + Sync
-      + 'static,
-  {
-    node.method = Some(Box::new(func));
-  }
+fn set_func(node: &mut Node, func: StoreFunc) {
+  node.method = Some(func);
+}
 
-  // can be optimized
-  fn loop_add(node: &mut Node, path: String) {
-    match &mut node.children {
-      Some(children) => match children.get(path.as_str()) {
-        Some(_) => {}
-        None => {
-          children.insert(
-            path,
-            Node {
-              method: None,
-              children: None,
-            },
-          );
-        }
+fn add_node(node: &mut Node, path: &'static str) {
+  // if no children exist then create new one
+  if node.children.is_none() {
+    let mut hash = hashbrown::HashMap::new();
+    hash.insert(
+      path,
+      Node {
+        method: None,
+        children: None,
       },
-      None => {
-        let mut hash = hashbrown::HashMap::new();
-        hash.insert(
-          path,
-          Node {
-            method: None,
-            children: None,
-          },
-        );
+    );
 
-        node.children = Some(hash);
-      }
+    node.children = Some(hash);
+  } else {
+    // if we have children then reuse existing or add new
+    let children = node.children.as_mut().unwrap();
+    if children.get(path).is_none() {
+      children.insert(
+        path,
+        Node {
+          method: None,
+          children: None,
+        },
+      );
     }
-  }
-}
-
-///
-///
-///
-///
-///
-///
-/// IGNORE OLD IMPLEMNTATION
-///
-///
-///
-///
-///
-///
-// Old router implementation
-pub struct Router2 {
-  // pub maps: Node,
-  pub func: Option<StoreFunc>,
-}
-
-impl<'a> Router2 {
-  pub fn new() -> Router2 {
-    Router2 {
-      // maps: Node {
-      //   path: String::from("MYPATH"),
-      //   children: None,
-      // },
-      func: None,
-    }
-  }
-
-  pub fn get<F>(mut self, func: F) -> Router2
-  where
-    F: Fn(request::Request) -> Box<Future<Item = response::Response, Error = ()> + Send + Sync>
-      + Send
-      + Sync
-      + 'static,
-  {
-    self.func = Some(Box::new(func));
-    self
-  }
-
-  pub fn build(self) -> Arc<Self> {
-    Arc::new(self)
   }
 }
